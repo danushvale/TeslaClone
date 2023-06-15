557200d8c8983a95018fdf2f973f2704bdf2651b
danushvale <119531393+danushvale@users.noreply.github.com>
2023-06-15 20:34:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+                <Header/>
+                <Home/>
+            </header>
+    </div>
+  );
+}
+
+export default App;
